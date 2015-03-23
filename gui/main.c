ada38aabe7d46dafa41e0e7b8746dde5fe29a136
mjpMint <mjp014@ucsd.edu>
2015-03-23 16:21:20 -0700

    gui now functional

--- gui/main.c
+++ gui/main.c
@@ -1,4 +1,5 @@
 #include <gtk/gtk.h>
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,27 +10,30 @@
 struct Coord {
   int row;
   int col;
-  struct Coord *next;
 };
 
-struct CoordList {
-  struct Coord *data;
-  struct CoordList *next;
+struct CoordPairList {
+  struct Coord start;
+  struct Coord end;
+  struct CoordPairList *next;
 };
 
 struct Board {
   int width;
   int height;
   char *content;
-  struct CoordList *coords;
+  struct CoordPairList *coords;
 };
 
 static struct Board *newBoard(FILE *, FILE *);
 static void deleteBoard(struct Board **);
-static struct CoordList *newCoordList(FILE *);
-static void deleteCoordList(struct CoordList **);
+static struct Coord readCoord(char *);
+static struct CoordPairList *newCoordPairList(FILE *);
+static void deleteCoordPairList(struct CoordPairList **);
+static FILE *printCoordPairList(struct CoordPairList *, FILE *);
 
 static gboolean cb_draw(GtkWidget *, cairo_t *, gpointer);
+static void drawBound(cairo_t *, struct Coord, struct Coord, double, double);
 static void create_controls(struct Board *);
 
 static struct Board *newBoard(FILE *board_input, FILE *answer)
@@ -53,7 +57,7 @@ static struct Board *newBoard(FILE *board_input, FILE *answer)
   }
   free(buf);
 
-  board->coords = newCoordList(answer);
+  board->coords = newCoordPairList(answer);
 
   return board;
 }
@@ -64,19 +68,76 @@ static void deleteBoard(struct Board **bpp)
 
   board = *bpp;
   free(board->content);
-  deleteCoordList(&board->coords);
+  board->content = NULL;
+  deleteCoordPairList(&board->coords);
 
   free(*bpp);
   *bpp = NULL;
 }
 
-static struct CoordList *newCoordList(FILE *answer)
+static struct Coord readCoord(char *left)
 {
-  return NULL;
+  struct Coord coord;
+  char *comma, *right;
+  comma = strchr(left, ',');
+  right = strchr(left, ')');
+  *comma = *right = '\0';
+  coord.row = atoi(left + 1);
+  coord.col = atoi(comma + 1);
+  return coord;
 }
 
-static void deleteCoordList(struct CoordList **lpp)
+static struct CoordPairList *newCoordPairList(FILE *answer)
 {
+  struct CoordPairList *root, *working;
+  char buf[BUFSIZ] = {0};
+  char *start, *end;
+
+  root = (struct CoordPairList *)malloc(sizeof(struct CoordPairList));
+  fgets(buf, BUFSIZ, answer);
+  start = strchr(buf, '(');
+  end = strrchr(buf, '(');
+  root->start = readCoord(start);
+  root->end = readCoord(end);
+  root->next = NULL;
+  working = root;
+  fgets(buf, BUFSIZ, answer);
+  while (!feof(answer))
+  {
+    working->next = (struct CoordPairList *)malloc(sizeof(struct CoordPairList));
+    start = strchr(buf, '(');
+    end = strrchr(buf, '(');
+    working = working->next;
+    working->start = readCoord(start);
+    working->end = readCoord(end);
+    working->next = NULL;
+
+    fgets(buf, BUFSIZ, answer);
+  }
+  return root;
+}
+
+static void deleteCoordPairList(struct CoordPairList **lpp)
+{
+  struct CoordPairList *list;
+  if (lpp == NULL || *lpp == NULL)
+  {
+    return;
+  }
+  list = *lpp;
+  deleteCoordPairList(&list->next);
+  free(*lpp);
+  *lpp = NULL;
+}
+
+static FILE *printCoordPairList(struct CoordPairList *coords, FILE *stream)
+{
+  while (coords)
+  {
+    fprintf(stream, "(%d,%d) (%d,%d)\n", coords->start.row, coords->start.col, coords->end.row, coords->end.col);
+    coords = coords->next;
+  }
+  return stream;
 }
 
 static gboolean cb_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
@@ -87,6 +148,7 @@ static gboolean cb_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
   cairo_font_extents_t fe;
   cairo_text_extents_t te;
   struct Board *board_info;
+  struct CoordPairList *coords;
   char letter[2];
 
   canvas_width = gtk_widget_get_allocated_width(widget);
@@ -103,6 +165,7 @@ static gboolean cb_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
   board_info = (struct Board *)data;
   m = board_info->height;
   n = board_info->width;
+  coords = board_info->coords;
 
   cairo_font_extents(cr, &fe);
   x = (canvas_width * px - n * fe.max_x_advance) / 2.0;
@@ -117,12 +180,113 @@ static gboolean cb_draw(GtkWidget *widget, cairo_t *cr, gpointer data)
 
       cairo_text_extents(cr, letter, &te);
       cairo_move_to(cr,
-          x + j * fe.max_x_advance - te.x_bearing - te.width / 2,
-          y + i * fe.max_x_advance - fe.descent + fe.height / 2);
+          x + j * fe.max_x_advance - te.x_bearing - te.width / 2.0,
+          y + i * fe.max_x_advance);
       cairo_show_text(cr, letter);
     }
   }
+
+  cairo_set_source_rgba(cr, 0.5, 0.0, 0.0, 0.5);
+  cairo_set_line_width(cr, px);
+
+  while (coords)
+  {
+    drawBound(cr, coords->start, coords->end, x, y);
+    coords = coords->next;
+  }
+
   return FALSE;
+}
+
+static void drawBound(cairo_t *cr, struct Coord start, struct Coord end,
+                                                      double x, double y)
+{
+  int x_disp, y_disp;
+  double ux, uy, px, size;
+  double ax, ay;
+  double radius, angle1, angle2, c1x, c1y, c2x, c2y, dist;
+  struct Coord pt1, pt2;
+  cairo_font_extents_t fe;
+
+  ux = uy = 1;
+  cairo_device_to_user_distance(cr, &ux, &uy);
+  px = ux > uy ? ux : uy;
+
+  cairo_font_extents(cr, &fe);
+  size = fe.max_x_advance;
+
+  radius = 8 * px;
+  angle1 = 0;
+  angle2 = 2 * M_PI;
+
+  if (start.col < end.col)
+  {
+    pt1 = start;
+    pt2 = end;
+  }
+  else if (start.col == end.col)
+  {
+    if (start.row < end.row)
+    {
+      pt1 = start;
+      pt2 = end;
+    }
+    else
+    {
+      pt1 = end;
+      pt2 = start;
+    }
+  }
+  else
+  {
+    pt1 = end;
+    pt2 = start;
+  }
+
+  c1x = x + pt1.col * size;
+  c1y = y + pt1.row * size - fe.ascent / 2.0;
+  c2x = x + pt2.col * size;
+  c2y = y + pt2.row * size - fe.ascent / 2.0;
+  x_disp = pt2.col - pt1.col;
+  y_disp = pt2.row - pt1.row;
+  dist = radius / sqrt(2);
+
+  if (pt1.col < pt2.col && pt1.row == pt2.row)
+  {
+    ax = c1x;
+    ay = c1y + radius;
+    angle1 = M_PI / 2.0;
+    angle2 = 3.0 * M_PI / 2.0;
+  }
+  else if (pt1.col == pt2.col && pt1.row < pt2.row)
+  {
+    ax = c1x - radius;
+    ay = c1y;
+    angle1 = M_PI;
+    angle2 = 2.0 * M_PI;
+  }
+  else if (pt1.col < pt2.col && pt1.row < pt2.row)
+  {
+    ax = c1x - dist;
+    ay = c1y + dist;
+    angle1 = 3.0 * M_PI / 4.0;
+    angle2 = 7.0 * M_PI / 4.0;
+  }
+  else
+  {
+    ax = c1x + dist;
+    ay = c1y + dist;
+    angle1 = M_PI / 4.0;
+    angle2 = 5.0 * M_PI / 4.0;
+  }
+
+  cairo_move_to(cr, ax, ay);
+  cairo_arc(cr, c1x, c1y, radius, angle1, angle2);
+  cairo_rel_line_to(cr, x_disp * size, y_disp * size);
+  cairo_arc(cr, c2x, c2y, radius, angle2, angle1);
+  cairo_rel_line_to(cr, -x_disp * size, -y_disp * size);
+
+  cairo_stroke(cr);
 }
 
 static void create_controls(struct Board *board)
@@ -199,6 +363,5 @@ int main(int argc, char *argv[])
   gtk_main();
 
   deleteBoard(&board_info);
-
   return EXIT_SUCCESS;
 }
